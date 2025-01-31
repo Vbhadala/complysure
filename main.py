693f59c36e8f4392a6d30f75be3ce053dade517b
Vbhadala <49553287+Vbhadala@users.noreply.github.com>
2025-01-31 14:43:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, APIRouter, Request,HTTPException
 from fastapi.templating import Jinja2Templates
-
 from fastapi.responses import JSONResponse, HTMLResponse, FileResponse,PlainTextResponse
 
 from typing import Any, List
@@ -10,8 +9,6 @@ from utils import fetch_bse, fetch_nse
 
 
 app = FastAPI(title="Chat App", version="0.0.1")
-
-# Define the absolute path for the static directory
 templates_path = os.path.join(os.path.dirname(__file__), "templates")
 templates = Jinja2Templates(directory=templates_path)
 
@@ -24,6 +21,31 @@ async def root(request: Request):
         return templates.TemplateResponse("index.html", {"request": request, "data": data})    
     else:
         return templates.TemplateResponse("index.html", {
+            "request": request,
+            "data": {'error': 'No data available'}
+        })
+
+@app.get("/app", response_class=HTMLResponse)
+async def root_app(request: Request):
+
+    data = fetch_nse()
+    if data:
+        return templates.TemplateResponse("app.html", {"request": request, "data": data})    
+    else:
+        return templates.TemplateResponse("app.html", {
+            "request": request,
+            "data": {'error': 'No data available'}
+        })
+
+
+@app.get("/segment", response_class=HTMLResponse)
+async def root_segment(request: Request):
+
+    data = fetch_bse()
+    if data:
+        return templates.TemplateResponse("segment.html", {"request": request, "data": data})    
+    else:
+        return templates.TemplateResponse("segment.html", {
             "request": request,
             "data": {'error': 'No data available'}
         })
@@ -49,7 +71,3 @@ async def get_oc_data(request: Request, exchange:str):
     })
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    
